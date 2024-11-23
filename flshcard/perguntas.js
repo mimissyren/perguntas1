1f6afbfeb6d362be6d10203b765954f59f728600
miriadlm <89555530+mimissyren@users.noreply.github.com>
2024-11-22 21:35:40 -0300

    perguntas1

---
+++ flshcard/perguntas.js
@@ -0,0 +1,203 @@
+criaCartao(
+    'Barbie 01',
+    'Qual foi o ano de lançamento da primeira boneca Barbie?',
+    'A primeira boneca Barbie foi lançada em 9 de março de 1959, criada por Ruth Handler e produzida pela Mattel.'
+)
+
+criaCartao(
+    'Barbie 02',
+    'Barbie já teve filhos?',
+    'Não, Barbie nunca teve filhos oficialmente. Ela é retratada como uma mulher independente e já teve várias profissões e aventuras ao longo de sua trajetória.'
+)
+
+criaCartao(
+    'Barbie 03',
+    'Qual foi o primeiro namorado da Barbie?',
+    'O primeiro namorado da Barbie foi Ken, lançado em 1961, dois anos após a estreia da Barbie.'
+)
+
+criaCartao(
+    'Barbie 04',
+    'Quais são as irmãs da Barbie?',
+    'As irmãs de Barbie são Skipper (1964), Stacie (1992) e Chelsea (1995). Elas aparecem frequentemente em filmes e séries ao lado de Barbie.'
+)
+
+criaCartao(
+    'Barbiem 05',
+    'Barbie e Ken já terminaram? Se sim, por quanto tempo?',
+    'Sim, Barbie e Ken terminaram oficialmente em 2004, mas voltaram a ser um casal em 2011, após um intervalo de sete anos.'
+)
+
+criaCartao(
+    'Barbie 06',
+    'Quem são as melhores amigas da Barbie?',
+    'Barbie tem várias amigas ao longo dos anos. Algumas das mais conhecidas são Midge, Teresa, Nikki e Summer, que aparecem em filmes e séries.'
+)
+
+criaCartao(
+    'Barbie 07',
+    'Qual foi o primeiro filme da Barbie lançado em animação?',
+    'O primeiro filme animado da Barbie foi Barbie em O Quebra-Nozes, lançado em 2001.'
+)
+
+criaCartao(
+    'Barbie 08',
+    'Qual foi a primeira casa da Barbie e quando foi lançada?',
+    'A primeira casa da Barbie foi a “Barbie Dreamhouse”, lançada em 1962. Era uma casa compacta feita de papelão, refletindo o estilo da época.'
+)
+
+criaCartao(
+    'Barbie 09',
+    'Quais foram os primeiros filmes da Barbie lançados entre 2000 e 2010?',
+    'Os primeiros filmes da Barbie foram Barbie em O Quebra-Nozes (2001), Barbie em Rapunzel (2002), Barbie em O Lago dos Cisnes (2003), Barbie e a Princesa e a Plebéia (2004), Barbie e o Castelo de Diamante (2008), entre outros.'
+)
+
+criaCartao(
+    'Barbie 10',
+    'Qual é o nome verdadeiro da Barbie e quem inspirou seu nome?',
+    'O nome verdadeiro da Barbie é Barbara Millicent Roberts. Ela foi inspirada na filha de sua criadora, Ruth Handler, que se chamava Barbara.'
+)
+
+criaCartao(
+    'Barbie 11',
+    'Sobre a série Barbie: Life in the Dreamhouse, quantas temporadas foram lançadas e qual é a premissa da série?',
+    'Barbie: Life in the Dreamhouse teve 7 temporadas lançadas entre 2012 e 2015. A série é uma comédia satírica que segue Barbie e seus amigos vivendo em uma casa de sonhos cheia de tecnologia.'
+)
+
+criaCartao(
+    'Barbie 12',
+    'Quem são os fabricantes da Barbie?',
+    'A Barbie é fabricada pela Mattel, uma das maiores empresas de brinquedos do mundo.'
+)
+
+criaCartao(
+    'Barbie 13',
+    'Na série Barbie: Dreamhouse Adventures, quem são os personagens principais e como a série difere das outras?',
+    'Dreamhouse Adventures segue Barbie, suas irmãs (Skipper, Stacie e Chelsea) e seus amigos em aventuras mais "realistas" e cotidianas. A série se concentra mais em sua vida familiar e em seu relacionamento com Ken.'
+)
+
+criaCartao(
+    'Barbie 14',
+    'Qual foi o filme da Barbie que comemorou o 50º aniversário da personagem?',
+    'O filme que comemorou o 50º aniversário da Barbie foi Barbie e as Três Mosqueteiras, lançado em 2009.'
+)
+
+criaCartao(
+    'Barbie 15',
+    'Qual é o tema principal do filme Barbie em As 12 Princesas Bailarinas e em que ano ele foi lançado?',
+    ' Barbie em As 12 Princesas Bailarinas foi lançado em 2006 e tem como tema central o balé, com Barbie interpretando a princesa Genevieve, que junto com suas irmãs, usa a dança para salvar o reino.'
+)
+
+criaCartao(
+    'Barbie 16',
+    'Qual foi a primeira profissão de Barbie?',
+    ' A primeira profissão de Barbie foi modelo, refletindo sua aparência e estilo de vida glamoroso no início de sua carreira.'
+)
+
+criaCartao(
+    'Barbie 17',
+    'Qual filme da Barbie é uma adaptação de O Príncipe e o Mendigo, de Mark Twain?',
+    'Barbie em A Princesa e a Plebéia (2004) é uma adaptação solta de várias histórias de princesas e tem uma estrutura semelhante à de A Princesa e o Sapo, com duas protagonistas trocando de papéis.'
+)
+
+criaCartao(
+    'Barbie 18',
+    'Quantas profissões Barbie já teve ao longo dos anos?',
+    'Barbie já teve mais de 200 profissões diferentes, incluindo astronauta, médica, veterinária, presidente e engenheira.'
+)
+
+criaCartao(
+    'Barbie 19',
+    'Barbie já teve outro namorado além de Ken?',
+    'Sim, durante o rompimento com Ken entre 2004 e 2011, Barbie teve um breve relacionamento com um surfista chamado Blaine.'
+)
+
+criaCartao(
+    'Barbie 20',
+    'Quem é a principal inimiga da Barbie nos filmes?',
+    'Em alguns filmes, a antagonista mais notável é a Raquelle, especialmente na série Barbie: Life in the Dreamhouse, onde elas têm uma relação de rivalidade.'
+)
+
+criaCartao(
+    'Barbie 21',
+    'Quais são os pares românticos mais conhecidos da Barbie nos filmes?',
+    'Em muitos filmes, Ken é o par romântico da Barbie, mas em Barbie e o Castelo de Diamante, o personagem Ian também é um interesse romântico de Barbie (chamada Liana no filme). Em Barbie em A Sereia das Pérolas, o príncipe Nalu também aparece como uma figura próxima, interpretados por Blaine, seu namorado na época em que Barbie e Ken terminaram.'
+)
+
+criaCartao(
+    'Barbie 22',
+    'Barbie já foi presidente ou participou de algum cenário político?',
+    'Sim, Barbie já foi candidata à presidência dos EUA em várias edições da boneca, incluindo as linhas lançadas em 1992, 2004, 2012 e 2020.'
+)
+
+criaCartao(
+    'Barbie 23',
+    'Qual foi o primeiro filme onde as irmãs da Barbie apareceram juntas?',
+    'O primeiro filme onde as irmãs de Barbie tiveram papéis principais foi Barbie e as Suas Irmãs em Uma Aventura de Cavalos (2013).'
+)
+
+criaCartao(
+    'Barbie 24',
+    'Ken já interpretou outro personagem que não fosse Ken nos filmes?',
+    'Sim, Ken já interpretou personagens diferentes, como no filme Barbie em A Princesa e a Plebéia, onde ele foi o Rei Dominick. Também teve papéis em outros filmes, onde desempenha personagens que se assemelham a Ken, mas com identidades próprias.'
+)
+
+criaCartao(
+    'Barbie 25',
+    'Qual é o nome da série de brinquedos de moda da Barbie?',
+    'A principal linha de brinquedos de moda da Barbie é chamada Barbie Fashionistas, que celebra a diversidade e a inclusão com bonecas de diferentes corpos, tons de pele e estilos de moda.'
+)
+
+criaCartao(
+    'Barbie 26',
+    'Barbie já foi uma estrela do rock em algum filme?',
+    'Sim, no filme Barbie em Rock`n Royals (2015), Barbie se transforma em uma estrela do rock chamada Princess Courtney.'
+)
+
+criaCartao(
+    'Barbie 27',
+    'Quantas casas a Barbie já teve ao longo dos anos?',
+    'Barbie já teve mais de 10 versões da Dreamhouse, começando pela primeira em 1962, até as versões mais modernas e tecnológicas com elevadores e luzes.'
+)
+
+criaCartao(
+    'Barbie 28',
+    'Quem foi a pessoa real que inspirou o nome Barbie?',
+    'Barbie foi inspirada em Barbara Handler, filha de Ruth Handler, a criadora da boneca.'
+)
+
+criaCartao(
+    'Barbie 29',
+    'Qual é o nome do cavalo de Barbie no filme Barbie e as Suas Irmãs em Uma Aventura de Cavalos?',
+    'O cavalo de Barbie no filme é chamado Majesty.'
+)
+
+criaCartao(
+    'Barbie 30',
+    'Barbie já teve animais de estimação em seus filmes?',
+    'Sim, Barbie já teve muitos animais de estimação, incluindo cães, gatos, cavalos, e até um pavão no filme Barbie: Princesa da Ilha.'
+)
+
+criaCartao(
+    'Barbie 31',
+    'Barbie já foi atleta em algum filme?',
+    'Barbie não foi atleta no sentido tradicional de esportes competitivos, mas em Barbie e as 12 Princesas Bailarinas (2006), ela é uma dançarina talentosa, o que exige muita disciplina física, semelhante ao treinamento de atletas. No filme Barbie: Escola de Princesas (2011), quem joga futebol é a amiga de Barbie, Isla, e não Barbie.'
+)
+
+criaCartao(
+    'Barbie 32',
+    'Barbie já foi sereia em quantos filmes?',
+    'Barbie já apareceu como sereia em vários filmes, como Barbie em Vida de Sereia (2010), Barbie em A Sereia das Pérolas (2014), e outros.'
+)
+
+criaCartao(
+    'Barbie 33',
+    'Barbie já estrelou filmes de aventura no espaço?',
+    'Sim, em Barbie: Aventura nas Estrelas (2016), Barbie vai ao espaço em uma missão para salvar a galáxia.'
+)
+
+criaCartao(
+    'Barbie 34',
+    'Quantos anos tinha Barbie quando foi lançada?',
+    'Barbie foi lançada oficialmente em 9 de março de 1959, e sua idade oficial foi representada como 19 anos.'
+)
